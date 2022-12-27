05b26c74d21b2ac8dd428c445970c6c978a0cacb
IrinaDudko0712 <113918542+IrinaDudko0712@users.noreply.github.com>
2022-12-27 10:14:33 +0300

    Create bpm_22_3_5.cpp

---
+++ bpm_22_3_5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main() {
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	std::string s;
+	int massive = (a, b, c);
+	int end_a;
+	end_a = (a % 100) % 10;
+	int start_a;
+	start_a = a / 100;
+	int end_b;
+	end_b = (b % 100) % 10;
+	int start_b;
+	start_b = b / 100;
+	int end_c;
+	end_c = (c % 100) % 10;
+	int start_c;
+	start_c =  c/ 100;
+
+	std::cout << "" << std::endl;
+
+}
